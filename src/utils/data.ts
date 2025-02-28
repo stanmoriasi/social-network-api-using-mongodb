7b340077fbd9413138f52ddec8154f95aecbc14c
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-27 21:38:53 -0700

    add test seed data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,1 +1,25 @@
+export const userData = [{
+    username: 'lorem',
+    email: 'email@yaoo.com',
+    thought: ['lorem ipsum dolor sit amet','another thought' ]
+},
+{
+    username: 'lorem2',
+    email: 'sample@email.com',
+    thought: ['lorem ipsum dolor sit amet', 'another thought' ]
+},
+{
+    username: 'lorem3',
+    email: 'sample@email.com',
+    thought: ['lorem ipsum dolor sit amet', 'another thought' ],
+    friends: ['lorem2', 'lorem3']
+},
+{
+    username: 'lorem4',
+    email: 'sample@email.com',
+    thought: ['lorem ipsum dolor sit amet', 'another thought', 'lorem ipsum dolor sit amet', 'another thought' ],
+},
+
+]
+
 
